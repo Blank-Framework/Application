ad5dc609c049e3cd7364450d4854d42b88264e88
Adrian du Plessis <8139635+AdrianDUP@users.noreply.github.com>
2025-01-06 08:02:05 +0100

    Added Application class and functionality

---
+++ src/Application.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace BlankFramework\Application;
+
+use BlankFramework\ApplicationInterface\ApplicationInterface;
+use BlankFramework\RoutingInterfaces\SimpleRouterInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Log\LoggerInterface;
+
+class Application implements ApplicationInterface
+{
+    public function __construct(
+        private SimpleRouterInterface $router,
+        private LoggerInterface $logger,
+    ) {
+    }
+
+    public function run(RequestInterface $request): void
+    {
+        try {
+            $route = $this->router->routeRequest($request);
+            $response = $route->handleRequest($request);
+        } catch (\Throwable $throwable) {
+            $this->logger->error(
+                'Exception occurred while routing request',
+                [
+                    'errorMessage' => $throwable->getMessage(),
+                    'errorCode' => $throwable->getCode(),
+                ]
+            );
+        }
+    }
+
+    private function sendResponse(ResponseInterface $response): void
+    {
+        $this->setHeaders($response->getHeaders());
+        $this->setStatusCode($response->getStatusCode());
+        $this->sendBody($response->getBody());
+    }
+
+    /**
+     * @param array<string, string[]> $headers
+     */
+    private function setHeaders(array $headers): void
+    {
+        foreach ($headers as $header => $values) {
+            foreach ($values as $value) {
+                header(sprintf('%s: %s', $header, $value), false);
+            }
+        }
+    }
+
+    private function setStatusCode(int $statusCode): void
+    {
+        http_response_code($statusCode);
+    }
+
+    private function sendBody(StreamInterface $body): void
+    {
+        echo $body->getContents();
+    }
+}
